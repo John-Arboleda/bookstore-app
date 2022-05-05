d4125fc52e56f0ed2af988e21890eb6ffc82c600
John-Arboleda <johnjack0126@gmail.com>
2022-05-05 16:34:54 -0500

    Add functionality display bookItems

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 import NewBook from './NewBook';
 
 function Books() {
+  const books = useSelector((state) => state.booksReducer);
   return (
     <div>
       <ul>
-        <li><BookItem /></li>
+        {books.map((book) => (
+          <li><BookItem key={book.id} book={book} /></li>
+        ))}
+
       </ul>
       <NewBook />
     </div>

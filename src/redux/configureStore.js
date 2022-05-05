5f2a2edfe361d3fd0b8d63263a6b24f015713f70
John-Arboleda <johnjack0126@gmail.com>
2022-05-04 21:25:28 -0500

    Configure Redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { configureStore, combineReducers } from 'redux';
+import booksReducer from './books/books';
+
+const reducer = combineReducers({
+  booksReducer,
+  // additional reducers could be added here
+});
+
+const store = configureStore(
+  reducer
+);
+
+export default store;

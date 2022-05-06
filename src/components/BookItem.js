4baa4a50d029adfe8ba2eba1e263f243daf432c5
John-Arboleda <johnjack0126@gmail.com>
2022-05-05 19:02:59 -0500

    Add removeBooks feature

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 import './reset.css';
 import styles from './BookItem.module.css';
 
 function BookItem({ book }) {
+  const dispatch = useDispatch();
+  const removeHandler = () => {
+    dispatch(removeBook(book.id));
+  };
+
   return (
     <div>
     <div className={styles.bookItem}>
@@ -11,7 +18,7 @@ function BookItem({ book }) {
       <p className={styles.itemAuthor}>{book.author}</p>
       <ul className={styles.itemActions}>
         <li><button type="button" onClick="">Comments</button></li>
-        <li><button type="button" onClick="">Remove</button></li>
+        <li><button type="button" onClick={removeHandler}>Remove</button></li>
         <li><button type="button" onClick="">Edit</button></li>
       </ul>
     </div>

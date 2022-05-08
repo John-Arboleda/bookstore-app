53448162a994ce7a2222c7343c96871cdfbad3fc
John-Arboleda <johnjack0126@gmail.com>
2022-05-07 20:26:35 -0500

    Style BookItems

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBookAsync } from '../redux/books/books';
-import './reset.css';
-import styles from './BookItem.module.css';
+import styles from '../styles/BookItem.module.css';
 
 function BookItem({ book }) {
   const dispatch = useDispatch();
@@ -12,17 +11,33 @@ function BookItem({ book }) {
   };
 
   return (
-    <div>
-    <div className={styles.bookItem}>
+    <div className={styles.container}>
+    <div className={styles.bookInfo}>
       <p className={styles.itemCategory}>{book.category}</p>
-      <h2 className={styles.itemTitle}>{book.title}</h2>
-      {/* <p className={styles.itemAuthor}>{book.author}</p> */}
-      <ul className={styles.itemActions}>
-        <li><button type="button" onClick="">Comments</button></li>
-        <li><button type="button" onClick={removeHandler}>Remove</button></li>
-        <li><button type="button" onClick="">Edit</button></li>
+      <h2 className={styles.title}>{book.title}</h2>
+      <p className={styles.author}>Suzanne Collins</p>
+      <ul className={styles.interfaceBtns}>
+        <li><button type="button" className={styles.interfaceButton} onClick="">Comments</button></li>
+        <li><button type="button" className={styles.interfaceButton} onClick={removeHandler}>Remove</button></li>
+        <li><button type="button" className={styles.interfaceButton} onClick="">Edit</button></li>
       </ul>
     </div>
+    <div className={styles.contentRight}>
+        <div className={styles.progress}>
+          <div className={styles.progress_outer}>
+            <div className={styles.progress_inner} />
+          </div>
+          <div className={styles.percentText}>
+            <span className={styles.percent}>0%</span>
+            <span className={styles.completed}>completed</span>
+          </div>
+        </div>
+        <div className={styles.bookMark}>
+          <span className={styles.currentChapter}>CURRENT CHAPTER</span>
+          <span className={styles.chapterNum}>Chapter 1</span>
+          <button type="button" className={styles.updateProgress}>UPDATE PROGRESS</button>
+        </div>
+      </div>
     </div>
   );
 }
